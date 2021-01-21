17183927c41d18283cd0967d0ebb21531b93868b
AJ Pryor <apryor6@gmail.com>
2021-01-21 07:58:50 -0500

    Tweak timings

--- gui/www/js/animate.js
+++ gui/www/js/animate.js
@@ -1,5 +1,5 @@
 (function () {
   console.log("HELLO");
-  gsap.from("text,#flame", { x: -100, stagger: 0.05 }, 250);
-  gsap.from("#main-subtitle", { opacity: 0 }, 1000);
+  gsap.from("text,#flame", { x: -100, stagger: 0.05, duration: 0.5 });
+  gsap.from("#main-subtitle", { opacity: 0, delay: 1.25 });
 })();
